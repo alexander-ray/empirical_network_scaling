3576eacd6f896b1a4ebe5b7f4abcbdadacbf633e
alexander-ray <1alexray@gmail.com>
2019-08-29 18:46:58 -0600

    Added ER generator

--- py_tools/src/runner.py
+++ py_tools/src/runner.py
@@ -2,50 +2,26 @@ import sys
 import time
 import numpy as np
 import csv
-from src.graph import semisparse as ss
-from src.graph import empirical
-from src.graph import configuration_model_mcmc
-from src.multi import helper as mh
-from src.graph import pathsample as ps
-from src.graph.apsp_multi_component import apsp_multi_component
-from src.utils.dict_statistics import *
+sys.path.append('/Users/alexray/Dropbox/graph_scaling/empirical_network_scaling')
+from empirical_network_scaling.py_tools.src.graph import semisparse as ss
+from empirical_network_scaling.py_tools.src.graph import empirical
+from empirical_network_scaling.py_tools.src.graph import configuration_model_mcmc
+from empirical_network_scaling.py_tools.src.graph import degree_aware_des
+from empirical_network_scaling.py_tools.src.multi import helper as mh
+from empirical_network_scaling.py_tools.src.graph import pathsample as ps
+from empirical_network_scaling.py_tools.src.graph.apsp_multi_component import apsp_multi_component
+from empirical_network_scaling.py_tools.src.utils.dict_statistics import *
 from abc import ABC, abstractclassmethod
 import os
+import igraph
 from scipy.stats import sem
+
 
 class NetworkStatisticGenerator(ABC):
     @staticmethod
     @abstractclassmethod
     def generate(filename):
         pass
-
-
-class BruteMGDGenerator(NetworkStatisticGenerator):
-    @staticmethod
-    def generate(source):
-        if type(source) == str:
-            G = empirical.networkx_from_gml(source)
-        else:
-            G = source
-
-        n = len(G)
-        degrees = list(sorted([degree[1] for degree in G.degree]))
-        c = sum(degrees) / len(G)
-
-        num_distances, sum_distances = ss.all_pairs_shortest_paths_rolling_sum(G)
-        if num_distances == 0:
-            mgd = 0
-        else:
-            mgd = sum_distances / num_distances
-
-        # Print info for tracking after calculation has been completed
-        print([source, n, c, mgd])
-        sys.stdout.flush()
-
-        if type(source) == str:
-            return [[source, n, c, mgd]]
-        else:
-            return [['', n, c, mgd]]
 
 
 class DistanceDistributionGenerator(NetworkStatisticGenerator):
@@ -148,25 +124,29 @@ class MeanDegreeGenerator(NetworkStatisticGenerator):
             return [['', n, c]]
 
 
-class MCMCGenerator(NetworkStatisticGenerator):
+class ErdosRenyiGenerator(NetworkStatisticGenerator):
     @staticmethod
     def generate(source):
         ret = []
         ret.append(source)
 
-        g = empirical.igraph_from_gml(source)
-        sampler = configuration_model_mcmc.MCMCSampler(g=g, burn_swaps=None,
-                                                       convergence_threshold=0.05, mixing_swaps=None)
+        print('graph: ' + source)
+        sys.stdout.flush()
 
+        g = empirical.igraph_from_gml(source)
         n = g.vcount()
+        m = g.ecount()
         degrees = list(sorted([d for d in g.degree(mode='ALL', loops=False)]))
         c = sum(degrees) / n
 
         ret.append(n)
         ret.append(c)
 
+        print(ret)
+        sys.stdout.flush()
+
         for _ in range(20):
-            g = sampler.get_new_sample()
+            g = igraph.GraphBase.Erdos_Renyi(n=n, m=m, directed=False, loops=False)
             if n < 1000:
                 dist = apsp_multi_component(g)
                 mgd = mean_of_dict(dist)
@@ -188,47 +168,65 @@ class MCMCGenerator(NetworkStatisticGenerator):
         return [ret]
 
 
-class TestGenerator(NetworkStatisticGenerator):
+class MCMCGenerator(NetworkStatisticGenerator):
     @staticmethod
     def generate(source):
         ret = []
         ret.append(source)
-        start = time.time()
+
+        print('graph: ' + source)
+        sys.stdout.flush()
+
         g = empirical.igraph_from_gml(source)
-        end = time.time()
-        network_read_time = end-start
+        #sampler = configuration_model_mcmc.MCMCSampler(g=g, burn_swaps=None,
+        #                                               convergence_threshold=0.05, mixing_swaps=None)
+        sampler = degree_aware_des.MCMCSampler(g=g, burn_swaps=None, convergence_threshold=0.1, mixing_swaps=None)
+        n = g.vcount()
+        degrees = list(sorted([d for d in g.degree(mode='ALL', loops=False)]))
+        c = sum(degrees) / n
 
-        start = time.time()
-        samples = ps.threshold_sampler_igraph(g, threshold=1, batch_size=10000)
-        end = time.time()
-        sampler_time = end-start
-
-        ret.append(network_read_time)
-        ret.append(sampler_time)
-        ret.append(len(samples))
-        ret.append(np.mean(samples))
-        ret.append(np.std(samples, ddof=1))
-        ret.append(sem(samples, ddof=1))
+        ret.append(n)
+        ret.append(c)
 
         print(ret)
         sys.stdout.flush()
 
+        for _ in range(10):
+            #g = sampler.get_new_sample(p=0.2)
+            g = sampler.get_new_sample(p=0.4)
+            if n < 1000:
+                dist = apsp_multi_component(g)
+                mgd = mean_of_dict(dist)
+                median = median_of_dict(dist)
+                max_val = max_key_of_dict(dist)
+            else:
+                samples = ps.threshold_sampler_igraph(g, threshold=0.1, batch_size=1000)
+                mgd = np.mean(samples)
+                median = np.median(samples)
+                max_val = np.max(samples)
+            ret.append(mgd)
+            ret.append(median)
+            ret.append(max_val)
+            ret.append(g.transitivity_undirected(mode='zero'))
+            ret.append(g.assortativity_degree(directed=False))
+
+        print(ret)
+        sys.stdout.flush()
         return [ret]
 
-'''
+
 path_prepend = '/scratch/Users/alra7284/gmls/Anna/'
 paths = []
-with open('random_networks.txt', 'r') as f:
+with open('/Users/alra7284/random_networks.txt', 'r') as f:
     for line in f:
-        if not line.strip().endswith('n6.gml') and not line.strip().endswith('n5.gml'):
-            paths.append(path_prepend + line.strip())
+        #if not line.strip().endswith('n6.gml') and not line.strip().endswith('n5.gml'):
+        paths.append(path_prepend + line.strip())
 
 
 print('Num networks: ' + str(len(paths)))
 print('Num CPUs: ' + str(int(os.environ['SLURM_JOB_CPUS_PER_NODE'])))
 
-mh.multiprocessing_to_csv(BruteMGDGenerator.generate,
+mh.multiprocessing_to_csv(DegreeAssortativityGenerator.generate,
                           paths,
-                          'output_mgd_check_w_new_read_function.csv',
+                          f'/Users/alra7284/assortativity_{len(paths)}.csv',
                           int(os.environ['SLURM_JOB_CPUS_PER_NODE']))
-'''
